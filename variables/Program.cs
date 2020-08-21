eb862f516e7276800bdd044f8265df0761cd1b67
A'Darius Craig <adariusc03@gmail.com>
2020-08-21 14:54:31 -0500

    Get started

---
+++ variables/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dogName = "Chica";
+            int dogAge = 2;
+            char firstLetter = 'C';
+            bool isNice = true;
+            double bone = 3;
+            decimal weight = 45m;
+
+            Console.WriteLine($"Hell {dogName}. \n" +
+                $"Welcome to the vet. \n" +
+                $"Says here you are {dogAge} and you weigh {weight}lbs.\n" +
+                $"{isNice}, you are a nice dog. \n" +
+                $"Since the first letter in your name is {firstLetter}.\n" +
+                $"We will give you {bone} bones today");
+        }
+    }
+}
